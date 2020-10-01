3a66501eafc0dc4d4ed9680af15d43d9b895bf21
EharaSuzuna <31155@ojs.ac.jp>
2020-10-01 11:57:30 +0900

    4.1

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,27 +7,9 @@ using System.Threading.Tasks;
 namespace Chapter4 {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine(GetProduct());
-
 		}
-		private static string GetProduct() {
-			Sale sale = new Sale {
-				ShopName = "pet store",
-				Amount = 10000,
-				Product = "food"
-			};
-			return sale?.Product;
-
-		}
-		
 	}
-	class Sale {
-		//店舗名
-		public string ShopName { get; set; }
-		//売上高
-		public int Amount { get; set; }
-		public string Product { get; set; }
-	}
+	
 }
 
 
---
+++ Chapter4/YearMonth.cs
@@ -0,0 +1,22 @@
+﻿using Chapter4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4 {
+	class YearMonth {
+		public int Year { get; private set; }
+		public int Month { get; private set; }
+
+		public YearMonth(int year, int Month) {
+			this.Year = year;
+			this.Month = Month;
+		}
+
+	}
+}
+	
+
+
