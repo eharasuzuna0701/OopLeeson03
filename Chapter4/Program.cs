91f331fb674c14bbefded4287674f000b47b6ef2
EharaSuzuna <31155@ojs.ac.jp>
2020-10-01 10:21:51 +0900

    P116

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace Chapter4 {
 	class Program {
 		static void Main(string[] args) {
+			string code = "12345";
+
+			var message = GetMessage(code) ?? DefaltMessage();
+			Console.WriteLine(message);
+		}
+
+		private static object DefaltMessage() {
+			return "babu";
+		}
+
+		private static object GetMessage(string code) {
+			return "pien";
 		}
 	}
 }
