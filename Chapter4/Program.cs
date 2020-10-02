f3aa68de07b572f7f2dfbba07d007a926eacd9b8
EharaSuzuna <31155@ojs.ac.jp>
2020-10-02 15:05:49 +0900

    chapter4 練習問題

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,9 +7,62 @@ using System.Threading.Tasks;
 namespace Chapter4 {
 	class Program {
 		static void Main(string[] args) {
+			//4.1.1
+			var ym = new YearMonth[] {
+			new YearMonth(1980,1),
+			new YearMonth(1982,3),
+			new YearMonth(1984,5),
+			new YearMonth(1986,7),
+			new YearMonth(2008,9),
+			};
+			Console.WriteLine("___4.2.2___");
+			Exercise2_2(ym);
+			Console.WriteLine("___4.2.3___");
+			Console.WriteLine(FindFirst21C(ym));
+			Console.WriteLine("___4.2.4___");
+			Exercise2_4(ym);
+			Console.WriteLine("___4.2.5___");
+			Exercise2_5(ym);
 		}
+
+		//4.2.2
+		public static void Exercise2_2(YearMonth[] ym) {
+			foreach (var num in ym) {
+				Console.WriteLine(num);
+			}
+		}
+
+		//4.2.3
+		static YearMonth FindFirst21C(YearMonth[] yms) {
+
+			foreach (var ym in yms) {
+
+				if (ym.Is21Century) {
+					return ym;
+
+				}
+			}
+			return null;
+		}
+		//4.2.4
+		private static void Exercise2_4(YearMonth[] ym) {
+			var ymonth = FindFirst21C(ym);
+			if (ymonth == null) {
+				Console.WriteLine("21世紀のデータはありません");
+			} else {
+				Console.WriteLine(ym);
+			}
+
+		}
+		//4.2.5
+		private static void Exercise2_5(YearMonth[] ym) {
+			var array = ym.Select(ymo => ymo.AddOneMoth()).ToArray();
+			foreach (var ymo in array) {
+				Console.WriteLine(ymo);
+			}
+		}
+
 	}
-	
 }
 
 
--- Chapter4/YearMonth.cs
+++ Chapter4/YearMonth.cs
@@ -7,16 +7,43 @@ using System.Threading.Tasks;
 
 namespace Chapter4 {
 	class YearMonth {
+		#region 4.1.1
+		//
 		public int Year { get; private set; }
 		public int Month { get; private set; }
-
-		public YearMonth(int year, int Month) {
+		//コンストラクタ
+		public YearMonth(int year, int month) {
 			this.Year = year;
-			this.Month = Month;
+			this.Month = month;
 		}
 
+		#endregion
+		#region 4.1.2
+		public bool Is21Century {
+			get {
+				return 2001 <= Year && Year <= 2100;
+				}
+			}
+		#endregion
+		# region 4.1.3
+		public YearMonth AddOneMoth() {
+			if (this.Month == 12) {
+				this.Year = Year + 1; this.Month = 1;
+			} else {
+				this.Month = this.Month + 1;
+			}
+			return new YearMonth(this.Year, this.Month);
+		}
+		#endregion
+		#region 4.1.4
+		public override string ToString() {
+			return $"{Year}年{Month}月";
+		}
+		#endregion
 	}
+
 }
+
 	
 
 
