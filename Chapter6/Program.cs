f9d8557d3b2b1d810ac93555202d6be3f3d4103a
EharaSuzuna <31155@ojs.ac.jp>
2020-10-05 15:22:46 +0900

    p162

---
+++ Chapter6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6 {
+	class Program {
+		static void Main(string[] args) {
+			var numbar = Enumerable.Repeat(-1, 20).ToArray();
+			foreach (var item in numbar) {
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
