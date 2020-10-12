43050925279721e38fce1a69070e8cde8cc083b3
EharaSuzuna <31155@ojs.ac.jp>
2020-10-12 15:58:13 +0900

    p195

--- Chapter7/MontDay.cs
+++ Chapter7/MontDay.cs
@@ -13,5 +13,15 @@ namespace Chapter7 {
 			this.Day = day;
 			this.Mouth = mouth;
 		}
+		public override bool Equals(object obj) {
+			var other = obj as MontDay;
+			if (other == null)
+				throw new ArgumentException();
+			return this.Day == other.Day && this.Mouth == other.Mouth;
+		}
+		public override int GetHashCode() {
+			var cord = Mouth.GetHashCode();
+			return base.GetHashCode()*31 + Day.GetHashCode();
+		}
 	}
 }
--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -1,7 +1,9 @@
 ﻿
 using Section01;
+using Section02;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,32 +11,20 @@ using System.Threading.Tasks;
 namespace Chapter7 {
 	class Program {
 		static void Main(string[] args) {
-			//var employeeDict = new Dictionary<int, Employee>(){
-			//	{ 100,new Employee(100,"清水遼久")},
-			//	{ 112,new Employee(112,"芹沢洋和")},
-			//	{ 125,new Employee(125,"岩瀬奈央子")},
-			//};
-
-			//var eployees = employeeDict.Where(emp => emp.Value.Id % 2== 0);
-			//foreach (var item in eployees) {
-			//	Console.WriteLine($"{item.Value.Name}");
-			//}
-			//リスト
-			var employee = new List<Employee>() {
-				new Employee(100,"清水遼久"),
-				new Employee(112,"芹沢洋和"),
-				new Employee(125,"岩瀬奈央子"),
-				new Employee(143,"山田太郎"),
-				new Employee(148,"池田太郎"),
-				new Employee(152,"高田太郎"),
-				new Employee(155,"石川幸也"),
-				new Employee(161,"中沢信成"),
-			};
-			var employeeDict = employee.ToDictionary(emp => emp.Id).ToList();
-
-			foreach (KeyValuePair< int,Employee> item in employeeDict) {
-				Console.WriteLine($"{item.Value.Id} {item.Value.Name}");
+			var lines = File.ReadAllLines("sample.txt");
+			var we = new WordsExtractor(lines);
+			foreach (var word in we.Extract()) {
+				Console.WriteLine(word);
 			}
+			
+			//var dict = new Dictionary<MontDay, string> {
+			//	{ new MontDay(3,5),"珊瑚の日"},
+			//	{ new MontDay(8,4),"箸の日"},
+			//	{ new MontDay(10,3),"登山の日"},
+			//};
+			//var md = new MontDay(8, 4);
+			//var s = dict[md];
+			//Console.WriteLine(s);
 		}
 		
 	}
