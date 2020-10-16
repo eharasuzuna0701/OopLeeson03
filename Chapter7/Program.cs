6b97a433b747c2e2b68b3cf10adc69420ec97b28
EharaSuzuna <31155@ojs.ac.jp>
2020-10-16 13:23:22 +0900

    これから7-1

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -11,12 +11,22 @@ using System.Threading.Tasks;
 namespace Chapter7 {
 	class Program {
 		static void Main(string[] args) {
-			var lines = File.ReadAllLines("sample.txt");
-			var we = new WordsExtractor(lines);
-			foreach (var word in we.Extract()) {
-				Console.WriteLine(word);
-			}
+
 			
+			Console.WriteLine("**********************\n* 辞書登録プログラム *\n**********************");
+			Console.WriteLine("1.登録 2.内容表示");
+			Console.Write(">");
+			string n = Console.ReadLine();
+			while (n == "1" || n == "2") {
+				DuplicateKeySample();
+			};
+
+			//var lines = File.ReadAllLines("sample.txt");
+			//var we = new WordsExtractor(lines);
+			//foreach (var word in we.Extract()) {
+			//	Console.WriteLine(word);
+			//}
+
 			//var dict = new Dictionary<MontDay, string> {
 			//	{ new MontDay(3,5),"珊瑚の日"},
 			//	{ new MontDay(8,4),"箸の日"},
@@ -25,7 +35,35 @@ namespace Chapter7 {
 			//var md = new MontDay(8, 4);
 			//var s = dict[md];
 			//Console.WriteLine(s);
+
 		}
-		
+
+		static public void DuplicateKeySample() {
+			// ディクショナリの初期化
+			var dict = new Dictionary<string, List<string>>() {
+			   //{ "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+			   //{ "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+			};
+
+			// ディクショナリに追加
+			Console.Write("KEYを入力:");
+			var key = Console.ReadLine();
+			Console.Write("VALUEを入力:");
+			var value = Console.ReadLine();
+			Console.WriteLine("登録しました!");
+			if (dict.ContainsKey(key)) {
+				dict[key].Add(value);
+			} else {
+				dict[key] = new List<string> { value };
+			}
+
+			// ディクショナリの内容を列挙
+			foreach (var item in dict) {
+				foreach (var term in item.Value) {
+					Console.WriteLine("{0} : {1}", item.Key, term);
+				}
+			}
+		}
+
 	}
 }
