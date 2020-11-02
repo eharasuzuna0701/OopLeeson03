389ade1cd2faba212fff2184f1086b643ec43203
EharaSuzuna <31155@ojs.ac.jp>
2020-11-02 13:55:58 +0900

    UI完成

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -21,5 +21,13 @@ namespace SendMailApp {
 		public MainWindow() {
 			InitializeComponent();
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e) {
+
+		}
 	}
 }
