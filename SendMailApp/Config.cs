ea852eed9db798e3746e3e22c0d8b9269d3f7a00
EharaSuzuna <31155@ojs.ac.jp>
2020-11-05 11:59:44 +0900

    UI途中　11/5

---
+++ SendMailApp/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMailApp {
+	class Config {
+		public string Smtp { get; set; }
+		public string MailAddress { get; set; }
+		public string PassWord { get; set; }
+		public int Port { get; set; }
+		public bool Ssl { get; set; }
+
+		public void DefaultSet()
+		{
+			Smtp = "smtp.gmail.com";
+			MailAddress = "ojsinfosys01@gmail.com";
+			PassWord = "ojsInfosys2020";
+			Port = 587;
+			Ssl = true;
+		}
+	}
+}
